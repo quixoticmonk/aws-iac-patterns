58cd09f18320136de4a692f6080dd614f98ed2f3
Troy Dieter <troydieter@gmail.com>
2022-02-09 23:52:15 -0500

    spiffed up

--- amazon-spa/terraform/spa.tf
+++ amazon-spa/terraform/spa.tf
@@ -22,7 +22,7 @@ variable "parent_zone" {
 # CloudFront function used for redirects
 module "lambda_at_edge" {
   source = "cloudposse/cloudfront-s3-cdn/aws//modules/lambda@edge"
-  name = "spa_index_redirect_${random_id.rando.hex}"
+  name   = "spa_index_redirect_${random_id.rando.hex}"
   tags   = local.common-tags
 
   functions = {
@@ -55,8 +55,38 @@ data "aws_cloudfront_cache_policy" "Managed-CachingOptimized" {
   name = "Managed-CachingOptimized"
 }
 
-data "aws_cloudfront_response_headers_policy" "Managed-SecurityHeadersPolicy" {
-  name = "Managed-SecurityHeadersPolicy"
+resource "aws_cloudfront_response_headers_policy" "sec_resp_headers" {
+  name    = "sec_resp_headers-${random_id.rando.hex}"
+  comment = "Designated for ${random_id.rando.hex}"
+
+  security_headers_config {
+    content_type_options {
+      override = true
+    }
+    frame_options {
+      frame_option = "DENY"
+      override     = true
+    }
+    referrer_policy {
+      referrer_policy = "same-origin"
+      override        = true
+    }
+    xss_protection {
+      mode_block = true
+      protection = true
+      override   = true
+    }
+    strict_transport_security {
+      access_control_max_age_sec = "63072000"
+      include_subdomains         = true
+      preload                    = true
+      override                   = true
+    }
+    content_security_policy {
+      content_security_policy = "frame-ancestors 'none'; default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"
+      override                = true
+    }
+  }
 }
 
 module "acm_request_certificate" {
@@ -75,11 +105,12 @@ module "cdn" {
   namespace                   = var.namespace
   stage                       = var.environment
   name                        = var.domain_name
+  comment                     = "Used with ${var.namespace}-${random_id.rando.hex}"
   aliases                     = ["www.${var.domain_name}", var.domain_name]
   dns_alias_enabled           = true
   parent_zone_id              = var.parent_zone
   cache_policy_id             = data.aws_cloudfront_cache_policy.Managed-CachingOptimized.id
-  response_headers_policy_id  = data.aws_cloudfront_response_headers_policy.Managed-SecurityHeadersPolicy.id
+  response_headers_policy_id  = aws_cloudfront_response_headers_policy.sec_resp_headers.id
   lambda_function_association = module.lambda_at_edge.lambda_function_association
   tags                        = local.common-tags
   acm_certificate_arn         = module.acm_request_certificate.arn
